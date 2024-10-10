bdb64346c9253eaf086137e401c3cbe3f0cd3163
Daniel Fildán <fildand@centrum.cz>
2024-10-10 11:40:37 +0200

    Ojejku

--- src/main/java/net/danygames2014/spookysquashlands/listener/WorldGenListener.java
+++ src/main/java/net/danygames2014/spookysquashlands/listener/WorldGenListener.java
@@ -77,7 +77,7 @@ public class WorldGenListener {
         provider.addBiome(spookyBiomes[0], 0.0F, 0.35F, 0, 1);
         provider.addBiome(spookyBiomes[1], 0.35F, 0.7F, 0, 1);
         provider.addBiome(spookyBiomes[2], 0.7F, 0.95F, 0, 1);
-        provider.addBiome(spookyBiomes[0], 0.95F, 1.0F, 0, 1);
+        provider.addBiome(spookyBiomes[3], 0.95F, 1.0F, 0, 1);
 
         BiomeAPI.addOverworldBiomeProvider(StationAPI.NAMESPACE.id("spooky_provider"), provider);
     }
