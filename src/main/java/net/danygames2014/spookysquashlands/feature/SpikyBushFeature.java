1ccb6ce5edcbd3432c10a9e0e3cc98df4af6ce8e
Daniel Fildán <fildand@centrum.cz>
2024-10-10 14:50:20 +0200

    [WIP] Spiky Bush Feature

--- src/main/java/net/danygames2014/spookysquashlands/feature/SpikyBushFeature.java
+++ src/main/java/net/danygames2014/spookysquashlands/feature/SpikyBushFeature.java
@@ -9,20 +9,39 @@ import java.util.Random;
 public class SpikyBushFeature extends Feature {
     @Override
     public boolean generate(World world, Random random, int x, int y, int z) {
-        if (random.nextInt(2) != 0) {
-            return false;
-        }
-
         if (!world.getBlockState(x, y - 1, z).isOf(Block.GRASS_BLOCK)) {
             return false;
         }
 
-        world.setBlockState(x, y, z, Block.LOG.getDefaultState());
-        world.setBlockState(x, y + 1, z, Block.LEAVES.getDefaultState());
-        world.setBlockState(x + 1, y, z, Block.LEAVES.getDefaultState());
-        world.setBlockState(x, y, z + 1, Block.LEAVES.getDefaultState());
-        world.setBlockState(x - 1, y, z, Block.LEAVES.getDefaultState());
-        world.setBlockState(x, y, z - 1, Block.LEAVES.getDefaultState());
+        setBlock(world, x, y, z, Block.LOG);
+        switch (random.nextInt(3)) {
+            case 1, 2:
+                createLayer(world, x, y, z, 5);
+
+            case 0:
+                createLayer(world, x, y, z, 3);
+                break;
+        }
+
         return true;
+    }
+
+    public void createLayer(World world, int genX, int genY, int genZ, int layerSize) {
+        int xStart = genX - ((layerSize - 1) / 2);
+        int zStart = genZ - ((layerSize - 1) / 2);
+
+        for (int x = 0; x < layerSize; x++) {
+            for (int z = 0; z < layerSize; z++) {
+                setBlock(world, xStart + x, genY, zStart + z, Block.LEAVES);
+            }
+        }
+
+    }
+
+    public void setBlock(World world, int x, int y, int z, Block block) {
+        int topY = world.getTopY(x, z);
+        if (world.getBlockState(x, topY, z).isAir()) {
+            world.setBlockState(x, topY, z, block.getDefaultState());
+        }
     }
 }
