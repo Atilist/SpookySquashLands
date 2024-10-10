1e247c274a53f128f92b501ac62b8b569844235c
Daniel Fildán <fildand@centrum.cz>
2024-10-10 11:37:13 +0200

    Spooky Forest tweaks and unfuwufking the lowobotowomiwed cowde

--- src/main/java/net/danygames2014/spookysquashlands/listener/WorldGenListener.java
+++ src/main/java/net/danygames2014/spookysquashlands/listener/WorldGenListener.java
@@ -1,12 +1,9 @@
 package net.danygames2014.spookysquashlands.listener;
 
-import net.danygames2014.spookysquashlands.feature.BushFeature;
-import net.danygames2014.spookysquashlands.feature.HugePumpkinFeature;
-import net.danygames2014.spookysquashlands.feature.LonelyOakTreeFeature;
+import net.danygames2014.spookysquashlands.feature.*;
 import net.mine_diver.unsafeevents.listener.EventListener;
 import net.minecraft.block.Block;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.gen.feature.BirchTreeFeature;
 import net.minecraft.world.gen.feature.LargeOakTreeFeature;
 import net.modificationstation.stationapi.api.StationAPI;
 import net.modificationstation.stationapi.api.event.world.biome.BiomeRegisterEvent;
@@ -14,22 +11,23 @@ import net.modificationstation.stationapi.api.event.worldgen.biome.BiomeProvider
 import net.modificationstation.stationapi.api.worldgen.BiomeAPI;
 import net.modificationstation.stationapi.api.worldgen.biome.BiomeBuilder;
 import net.modificationstation.stationapi.api.worldgen.biome.ClimateBiomeProvider;
+import net.modificationstation.stationapi.api.worldgen.feature.HeightScatterFeature;
 import net.modificationstation.stationapi.api.worldgen.feature.LeveledScatterFeature;
 import net.modificationstation.stationapi.api.worldgen.surface.SurfaceBuilder;
 import net.modificationstation.stationapi.api.worldgen.surface.SurfaceRule;
 
 public class WorldGenListener {
     private Biome[] spookyBiomes;
-
+    
     @EventListener
     public void registerBiomes(BiomeRegisterEvent event) {
-
         spookyBiomes = new Biome[4];
 
         // Spooky Tundra
         SurfaceRule tundraSurface = SurfaceBuilder.start(Block.SNOW_BLOCK).replace(Block.GRASS_BLOCK).build();
 
-        BiomeBuilder spookyBuilder = BiomeBuilder.start("Spooky Tundra");
+        BiomeBuilder spookyBuilder;
+        spookyBuilder = BiomeBuilder.start("Spooky Tundra");
         spookyBuilder.height(64, 128);
         spookyBuilder.snow(true);
         spookyBuilder.leavesColor(0xFF3A976D);
@@ -51,11 +49,14 @@ public class WorldGenListener {
 
         // Spooky Forest
         spookyBuilder = BiomeBuilder.start("Spooky Forest");
-        spookyBuilder.height(64, 128);
-        spookyBuilder.leavesColor(0xFF75B719);
-        spookyBuilder.grassColor(0xFF75B719);
-        spookyBuilder.fogColor(0xFFFFFFFF);
-        spookyBuilder.feature(new BirchTreeFeature());
+        spookyBuilder.height(64, 80);
+        spookyBuilder.leavesColor(0xFF353735);
+        spookyBuilder.grassColor(0xFF353735);
+        spookyBuilder.fogColor(0xFF202020);
+        spookyBuilder.feature(new HeightScatterFeature(new LargeOakTreeFeature(), 2));
+        spookyBuilder.feature(new HeightScatterFeature(new SpikyBushFeature(), 2));
+        spookyBuilder.feature(new GrassFeature(0, 16));
+        spookyBuilder.feature(new GrassFeature(1, 128));
         spookyBiomes[2] = spookyBuilder.build();
 
         // Spooky Jungle
@@ -68,122 +69,16 @@ public class WorldGenListener {
         spookyBiomes[3] = spookyBuilder.build();
     }
 
-    // This method has been lobotomized for scientific purposes
-    @
-            EventListener
-    public
-    void
-    registerRegions
-            (
-                    BiomeProviderRegisterEvent
-                            event
-            )
-    {
-        // Simple
-        // climate
-        // provider
-        // with
-        // biomes
-        // by
-        // temperature
-        ClimateBiomeProvider
-                provider
-                =
-                new
-                        ClimateBiomeProvider
-                        (
+    // This method has been unlobotomized for sanity purposes
+    @EventListener
+    public void registerRegions(BiomeProviderRegisterEvent event) {
+        // Simple climate provider with biomes by temperature
+        ClimateBiomeProvider provider = new ClimateBiomeProvider();
+        provider.addBiome(spookyBiomes[0], 0.0F, 0.35F, 0, 1);
+        provider.addBiome(spookyBiomes[1], 0.35F, 0.7F, 0, 1);
+        provider.addBiome(spookyBiomes[2], 0.7F, 0.95F, 0, 1);
+        provider.addBiome(spookyBiomes[0], 0.95F, 1.0F, 0, 1);
 
-                        )
-                ;
-        provider
-                .
-                addBiome
-                        (
-                                spookyBiomes
-                                        [
-                                                0
-                                        ]
-                                ,
-                                0
-                                ,
-                                0.35F
-                                ,
-                                0
-                                ,
-                                1
-                        )
-        ;
-        provider
-                .
-                addBiome
-                        (
-                                spookyBiomes
-                                        [
-                                                1
-                                        ]
-                                ,
-                                0.35F
-                                ,
-                                0.8F
-                                ,
-                                0
-                                ,
-                                1
-                        )
-        ;
-        provider
-                .
-                addBiome
-                        (
-                                spookyBiomes
-                                        [
-                                                2
-                                        ]
-                                ,
-                                0.8F
-                                ,
-                                0.95F
-                                ,
-                                0
-                                ,
-                                1
-                        )
-        ;
-        provider
-                .
-                addBiome
-                        (
-                                spookyBiomes
-                                        [
-                                                3
-                                        ]
-                                ,
-                                0.95F
-                                ,
-                                1
-                                ,
-                                0
-                                ,
-                                1
-                        )
-        ;
-        BiomeAPI
-                .
-                addOverworldBiomeProvider
-                        (
-                                StationAPI
-                                        .
-                                        NAMESPACE
-                                        .
-                                        id
-                                                (
-                                                        "spooky" +
-                                                                "_" +
-                                                                "provider"
-                                                )
-                                ,
-                                provider
-                        )
-        ;
+        BiomeAPI.addOverworldBiomeProvider(StationAPI.NAMESPACE.id("spooky_provider"), provider);
     }
 }
