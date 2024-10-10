3e91a9d8f8360303efc4bfc25a58506ed4019e68
Atilist <71230342+Atilist@users.noreply.github.com>
2024-10-10 11:35:26 +0200

    Improved Lonely Oak Trees
    Trees now have Branches with Leaf Crowns.
    Reduced Leaf Crown Size.

--- src/main/java/net/danygames2014/spookysquashlands/feature/LonelyOakTreeFeature.java
+++ src/main/java/net/danygames2014/spookysquashlands/feature/LonelyOakTreeFeature.java
@@ -22,15 +22,66 @@ public class LonelyOakTreeFeature extends Feature {
         for (int offsetY = y; offsetY <= treeHeight; offsetY++) {
             world.setBlock(x, offsetY, z, Block.LOG.id);
         }
-        for (int offsetX = x - 3; offsetX <= x + 3; offsetX++) {
-            for (int offsetY = treeHeight - 3; offsetY <= treeHeight + 3; offsetY++) {
-                for (int offsetZ = z - 3; offsetZ <= z + 3; offsetZ++) {
+        placeLeaves(world, random, x, treeHeight, z);
+        for (int i = 0; i < 2 + random.nextInt(6); i++) {
+            int branchX = x + random.nextInt(9) - 4;
+            int branchY = treeHeight + random.nextInt(9) - 4;
+            int branchZ = z + random.nextInt(9) - 4;
+            int branchBegin = y + random.nextInt(treeHeight - y);
+            // Approach X
+            int startX;
+            int endX;
+            if (branchX > x) {
+                startX = x;
+                endX = branchX;
+            } else {
+                startX = branchX;
+                endX = x;
+            }
+            for (int j = startX; j <= endX; j++) {
+                world.setBlock(j, branchBegin, z, Block.LOG.id);
+            }
+            // Approach Z
+            int startZ;
+            int endZ;
+            if (branchZ > z) {
+                startZ = z;
+                endZ = branchZ;
+            } else {
+                startZ = branchZ;
+                endZ = z;
+            }
+            for (int j = startZ; j <= endZ; j++) {
+                world.setBlock(branchX, branchBegin, j, Block.LOG.id);
+            }
+            // Approach Y
+            int startY;
+            int endY;
+            if (branchY > branchBegin) {
+                startY = branchBegin;
+                endY = branchY;
+            } else {
+                startY = branchY;
+                endY = branchBegin;
+            }
+            for (int j = startY; j <= endY; j++) {
+                world.setBlock(branchX, j, branchZ, Block.LOG.id);
+            }
+            // Place leaves at branch end
+            placeLeaves(world, random, branchX, branchY, branchZ);
+        }
+        return true;
+    }
+
+    private void placeLeaves(World world, Random random, int x, int y, int z) {
+        for (int offsetX = x - 2; offsetX <= x + 2; offsetX++) {
+            for (int offsetY = y - 2; offsetY <= y + 2; offsetY++) {
+                for (int offsetZ = z - 2; offsetZ <= z + 2; offsetZ++) {
                     if (world.getBlockId(offsetX, offsetY, offsetZ) == 0) {
                         world.setBlock(offsetX, offsetY, offsetZ, Block.LEAVES.id);
                     }
                 }
             }
         }
-        return true;
     }
 }
